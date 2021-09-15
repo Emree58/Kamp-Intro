efa9fca3e62a7d3b9251e64a9656ad73b617c5c2
Emre Arslan <arslann1907@hotmail.com>
2021-09-15 14:36:18 +0200

    Add project files.

---
+++ Kamp-Intro/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Kamp_Intro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety - tip guvenligi
+            string kategoriEtiketi = "Kategori";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = true;
+            double dolarDun = 7.55;
+            double dolarBugun = 7.55;
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalis butonu");
+            }
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artis butonu");
+            }
+            else
+            {
+                Console.WriteLine("Degismedi butonu");
+            }
+
+            if (sistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanici ayarlari butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giris yap butonu");
+            }
+
+        }
+    }
+}
